e3bb8fff40792bf4b585152444b7e8cb14c65898
maryemlakhouil <lkhwilmaryem@gmail.com>
2025-11-18 21:05:36 +0100

    commencer la partie js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,6 @@
+// ouvrir / Fermer Modal d'ajout
+
+const ovrirModal = document.getElementById("openModal");
+const fermerModal = document.getElementById("closeModal");
 const modal = document.getElementById("addWorkerModal");
-const openModalBtn = document.getElementById("openModal");
-const closeModalBtn = document.getElementById("closeModal");
 
-openModalBtn.onclick = () => modal.classList.remove("hidden");
-closeModalBtn.onclick = () => modal.classList.add("hidden");
-
-// Optional: click outside closes
-modal.onclick = (e) => {
-  if (e.target === modal) modal.classList.add("hidden");
-};
